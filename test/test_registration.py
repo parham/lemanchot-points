fb3487747cb1044cacb9d8385e64332e5f253114
Parham Nooralishahi <parham.nooralishahi@gmail.com>
2022-06-03 16:22:54 -0400

    update registration test

--- test/test_registration.py
+++ test/test_registration.py
@@ -49,32 +49,32 @@ class Test_Registration(unittest.TestCase):
     #         'PHM Visualization', 1024, 768
     #     )
 
-    def test_cpd_registration(self):
-        batch = RGBDnTBatch(
-            root_dir='/home/phm/GoogleDrive/Personal/Datasets/my-dataset/multi-modal/20210722_pipe_heating/vtd',
-            filenames= [
-                'vtd_1626967963384.mat', 'vtd_1626967965865.mat', 'vtd_1626967973439.mat' # 
-            ]
-        )
+    # def test_cpd_registration(self):
+    #     batch = RGBDnTBatch(
+    #         root_dir='/home/phm/GoogleDrive/Personal/Datasets/my-dataset/multi-modal/20210722_pipe_heating/vtd',
+    #         filenames= [
+    #             'vtd_1626967963384.mat', 'vtd_1626967965865.mat', 'vtd_1626967973439.mat' # 
+    #         ]
+    #     )
 
-        pipobj = Pipeline([
-            FilterDepthRange_Step(),
-            ConvertToPC_Step(
-                depth_params_file = '/home/phm/GoogleDrive/Personal/Datasets/my-dataset/multi-modal/20210722_pipe_heating/depth/camera_info.json',
-                data_batch_key = 'prp_frames'),
-            CPDRegistration_Step(
-                voxel_size = 0.05,
-                data_pcs_key='pcs'),
-        ])
+    #     pipobj = Pipeline([
+    #         FilterDepthRange_Step(),
+    #         ConvertToPC_Step(
+    #             depth_params_file = '/home/phm/GoogleDrive/Personal/Datasets/my-dataset/multi-modal/20210722_pipe_heating/depth/camera_info.json',
+    #             data_batch_key = 'prp_frames'),
+    #         CPDRegistration_Step(
+    #             voxel_size = 0.05,
+    #             data_pcs_key='pcs'),
+    #     ])
 
-        res = pipobj(batch)
-        res_pc = res['fused_pc']
+    #     res = pipobj(batch)
+    #     res_pc = res['fused_pc']
 
-        visualize_vtd(
-            res_pc,
-            load_pinhole('/home/phm/GoogleDrive/Personal/Datasets/my-dataset/multi-modal/20210722_pipe_heating/depth/camera_info.json'), 
-            'PHM Visualization', 1024, 768
-        )
+    #     visualize_vtd(
+    #         res_pc,
+    #         load_pinhole('/home/phm/GoogleDrive/Personal/Datasets/my-dataset/multi-modal/20210722_pipe_heating/depth/camera_info.json'), 
+    #         'PHM Visualization', 1024, 768
+    #     )
 
     # def test_manual_registration(self):
     #     # colored pointcloud registration
@@ -108,35 +108,35 @@ class Test_Registration(unittest.TestCase):
     #         'PHM Visualization', 1024, 768
     #     )
 
-    # def test_colored_icp_registration(self):
-    #     # colored pointcloud registration
-    #     # This is implementation of following paper
-    #     # J. Park, Q.-Y. Zhou, V. Koltun,
-    #     # Colored Point Cloud Registration Revisited, ICCV 2017
+    def test_colored_icp_registration(self):
+        # colored pointcloud registration
+        # This is implementation of following paper
+        # J. Park, Q.-Y. Zhou, V. Koltun,
+        # Colored Point Cloud Registration Revisited, ICCV 2017
 
-    #     batch = RGBDnTBatch(
-    #         root_dir='/home/phm/GoogleDrive/Personal/Datasets/my-dataset/multi-modal/20210722_pipe_heating/vtd',
-    #         filenames= [
-    #             'vtd_1626967963384.mat', 'vtd_1626967965865.mat', 'vtd_1626967973439.mat'
-    #         ]
-    #     )
+        batch = RGBDnTBatch(
+            root_dir='/home/phm/GoogleDrive/Personal/Datasets/my-dataset/multi-modal/20210722_pipe_heating/vtd',
+            filenames= [
+                'vtd_1626967963384.mat', 'vtd_1626967965865.mat', 'vtd_1626967973439.mat'
+            ]
+        )
 
-    #     pipobj = Pipeline([
-    #         FilterDepthRange_Step(),
-    #         ConvertToPC_Step(
-    #             data_batch_key = 'prp_frames',
-    #             depth_params_file='/home/phm/GoogleDrive/Personal/Datasets/my-dataset/multi-modal/20210722_pipe_heating/depth/camera_info.json'),
-    #         ColoredICPRegistar_Step(data_pcs_key='pcs'),
-    #     ])
+        pipobj = Pipeline([
+            FilterDepthRange_Step(),
+            ConvertToPC_Step(
+                data_batch_key = 'prp_frames',
+                depth_params_file='/home/phm/GoogleDrive/Personal/Datasets/my-dataset/multi-modal/20210722_pipe_heating/depth/camera_info.json'),
+            ColoredICPRegistar_Step(data_pcs_key='pcs'),
+        ])
 
-    #     res = pipobj(batch)
-    #     res_pc = res['fused_pc']
+        res = pipobj(batch)
+        res_pc = res['fused_pc']
 
-    #     visualize_vtd(
-    #         res_pc,
-    #         load_pinhole('/home/phm/GoogleDrive/Personal/Datasets/my-dataset/multi-modal/20210722_pipe_heating/depth/camera_info.json'), 
-    #         'PHM Visualization', 1024, 768
-    #     )
+        visualize_vtd(
+            res_pc,
+            load_pinhole('/home/phm/GoogleDrive/Personal/Datasets/my-dataset/multi-modal/20210722_pipe_heating/depth/camera_info.json'), 
+            'PHM Visualization', 1024, 768
+        )
 
 if __name__ == '__main__':
     unittest.main()
